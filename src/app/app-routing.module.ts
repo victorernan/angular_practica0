b478f02b9eb02f654a177d2a453d8fef2fd7744c
victor.ospina <victor.ospina.giraldo>
2020-03-15 19:23:29 +0100

    cambios ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './Views/home/home.component';
-import { CorreosRecibidosComponent } from './Views/correos-recibidos/correos-recibidos.component';
-import { EnviarComponent } from './Views/enviar/enviar.component';
-import { VisualizarCorreoComponent } from './Views/visualizar-correo/visualizar-correo.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'mails', component: CorreosRecibidosComponent },
-  { path: 'send', component: EnviarComponent },
-  { path: 'mail', component: VisualizarCorreoComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
-// Main files
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-// Components
 import { CorreoComponent } from './Components/correo/correo.component';
 
 // Modules
