18c030a0986a120d519113b437a96ace1ebd5025
victor.ospina <victor.ospina.giraldo>
2020-03-16 18:07:45 +0100

    texto por pantalla

--- src/app/Components/correo/correo.component.ts
+++ src/app/Components/correo/correo.component.ts
@@ -13,9 +13,9 @@ export class CorreoComponent implements OnInit {
   constructor() { 
     this.correo ={
       Titulo: "Titulo del mail",
-      cuerpo: "Cuerpo del mail",
-      emisor: "emisor",
-      destinatario: "destinatario" 
+      cuerpo: "Esta obra maestra de la crónica negra en la América profunda, ganadora de 2 Oscars®, es un espléndido thriller que consagró a los hermanos Coen como los mejores cineastas americanos contemporáneos. Un hombre apocado y tímido (William H. Macy), casado con la hija de un millonario que le impide disfrutar de su fortuna, decide contratar a dos delincuentes (Steve Buscemi y Peter Stormare) para que secuestren a su mujer con el fin de montar un negocio propio con el dinero del rescate. Pero, por una serie de azarosas circunstancias, al secuestro se suman tres brutales asesinatos, lo que obliga a una oficial de policía embarazada con mucho sentido común (Frances McDormand) a intervenir.",
+      emisor: "victor hernan",
+      destinatario: "nerea san juan" 
     }
   }
 
